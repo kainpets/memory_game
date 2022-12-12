4448a3136c2d8343d386a7e86b3602e5813962be
kainpets <pawelstepniak22@gmail.com>
2022-12-12 23:52:39 +0100

    add tailwindcss and postcss

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello</h1>
+    </>
+  );
 }
 
 export default App;
